a22043b0fcfa20b2e14aac42cea80c0673160bc0
BrettLennox <brettheaton@hotmail.com>
2022-10-27 14:10:53 +1100

    Code Commenting + Attack
    commented code and cleaned up slightly
    started adding attack behaviour

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -11,9 +11,8 @@ using Vector3 = UnityEngine.Vector3;
 public class EnemyMovement : MonoBehaviour
 {
     private Animator _animator;
-    private NavMeshAgent _agent;
     [SerializeField] private Transform _movePosition;
-    [SerializeField] private Vector3[] _pathCorners;
+    [SerializeField] private Vector3[] _pathCorners = new Vector3[10];
     [SerializeField] private NavMeshPath _path;
     [SerializeField] private float pathUpdateTimer = 1f;
     private float timer = Mathf.Infinity;
@@ -33,26 +32,142 @@ public class EnemyMovement : MonoBehaviour
     void Start()
     {
         _animator = GetComponent<Animator>();
-        _agent = GetComponent<NavMeshAgent>();
         _path = new NavMeshPath();
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-        if (timer >= pathUpdateTimer)
+        CalculatePath();
+        CalculateAndUpdateMovement();
+    }
+
+    private void CalculateAndUpdateMovement()
+    {
+        if (shouldMove)
         {
+            CalculateDistance();
+            IncrementPathpointIndex();
+
+            
+            if (IsFacing()) //if dotProd is greater then angle
+            {
+                //sets the animator float values to move the AI forward and prevent turning 
+                //damps the values to the new values over a period determined by dampTime variable 
+                //runs over Time.deltaTime
+                _animator.SetFloat("y", yMove, dampTime, Time.deltaTime);
+                _animator.SetFloat("x", 0f, dampTime, Time.deltaTime);
+                //sets checkedTurn to false to allow the AI to determine turn direction when dotProd is less than angle
+                checkedTurn = false;
+            }
+            else
+            {
+                if (!checkedTurn) //if checkedTurn is false
+                {
+                    //determines angle from transform forward and current pathCorners target
+                    //returns the value signed
+                    Vector3 from = _pathCorners[index] - transform.position;
+                    Vector3 to = transform.up;
+                    float turnAngle = Vector3.SignedAngle(from, to, transform.forward);
+                    if (turnAngle == 90f) //if turnAngle is 90f
+                    {
+                        turnDir = xMove; //turn right
+                    }
+                    else if (turnAngle == -90f) //if turn angle -90f
+                    {
+                        turnDir = -xMove; //turn left
+                    }
+                    checkedTurn = true;
+                }
+                //sets the animator float values to move the AI forward and prevent turning 
+                //damps the values to the new values over a period determined by dampTime variable 
+                //runs over Time.deltaTime
+                _animator.SetFloat("y", 0f, dampTime, Time.deltaTime);
+                _animator.SetFloat("x", turnDir, dampTime, Time.deltaTime);
+            }
+        }
+        //checks if shouldMove is false && distance between transform position and movePosition is greater than 0.5f
+        else if (!shouldMove && Vector3.Distance(transform.position, _movePosition.position) > 0.5f)
+        {
+            shouldMove = true; //sets shouldMove to false
+        }
+        else //if shouldMove is false
+        {
+            //sets the float values in animator to be 0 using Mathf.Smoothstep from the current value of the floats
+            //run over the duration of stepTime
+            _animator.SetFloat("y", Mathf.SmoothStep(_animator.GetFloat("y"), 0f, stepTime));
+            _animator.SetFloat("x", Mathf.SmoothStep(_animator.GetFloat("x"), 0f, stepTime));
+            if (IsFacing())
+            {
+                _animator.SetTrigger("attack");
+            }
+        }
+    }
+
+    private bool IsFacing()
+    {
+        Vector3 dirFromAToB = (_pathCorners[index] - transform.position).normalized; // gets the direction from A to B normalized
+        float dotProd = Vector3.Dot(dirFromAToB, transform.forward); //dot product from dirFromAToB and the transform.forward //1 being looking at //-1 being looking away
+        if(dotProd > angle)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    private void IncrementPathpointIndex()
+    {
+        if (Vector3.Distance(transform.position, _pathCorners[index]) < _moveOffset) //if the distance between this transform and the current pathCorner target is less than moveOffset
+        {
+            index++; //increment the index
+            if (index == _path.corners.Length) //if the index is equal to the legnth of the paths corners
+            {
+                shouldMove = false; //shouldMove is set to false
+            }
+        }
+    }
+
+    private void CalculateDistance()
+    {
+        //checks the distance between transform position and movePosition
+        //if value is less than moveOffset
+        if (Vector3.Distance(transform.position, _movePosition.position) < _moveOffset)
+        {
+            shouldMove = false; //shouldMove is set to false
+        }
+    }
+
+    private void CalculatePath()
+    {
+        //increments timer over Time.deltaTime
+        timer += Time.deltaTime;
+        if (timer >= pathUpdateTimer) //if timer is greater than or eual to pathUpdateTimer
+        {
+            //reset timer to 0
             timer = 0f;
+            //Calculates the path from transform position to movePosition
+            //using all NavMesh Areas
+            //returns the path data back to path variable
             NavMesh.CalculatePath(transform.position, _movePosition.position, NavMesh.AllAreas, _path);
+            //calculates the amount of corners in the path
+            //adjusts pathPointsCount value to be the amount of corners
+            //creates an array of corners Vector position and sets to pathCorners
             _pathPointsCount = _path.GetCornersNonAlloc(_pathCorners);
 
-            while (_pathCorners.Length <= _pathPointsCount)
+            while (_pathCorners.Length <= _pathPointsCount) //while pathCorners length is less than or equal to pathPointsCount
             {
+                //Resizes the array to allow for wiggle room when adding new members to the array
                 Array.Resize(ref _pathCorners, _pathCorners.Length * 2);
+                //recalculates corners and adds to array
                 _pathPointsCount = _path.GetCornersNonAlloc(_pathCorners);
             }
 
+#if UNITY_EDITOR
+            //whilst in UnityEditor
+            //creates a Debug Drawline between all path corners
             for (int i = 0; i < _path.corners.Length - 1; i++)
             {
                 if (i < _path.corners.Length)
@@ -62,59 +177,6 @@ public class EnemyMovement : MonoBehaviour
                 else { break; }
             }
         }
-
-        if (shouldMove)
-        {
-            if (Vector3.Distance(transform.position, _movePosition.position) < _moveOffset)
-            {
-                shouldMove = false;
-            }
-            Vector3 dirFromAToB = (_pathCorners[index] - transform.position).normalized;
-            float dotProd = Vector3.Dot(dirFromAToB, transform.forward);
-            if (Vector3.Distance(transform.position, _pathCorners[index]) < _moveOffset)
-            {
-                index++;
-                if (index == _path.corners.Length)
-                {
-                    shouldMove = false;
-                }
-            }
-
-            if (dotProd > angle)
-            {
-                _animator.SetFloat("y", yMove, dampTime, Time.deltaTime);
-                _animator.SetFloat("x", 0f, dampTime, Time.deltaTime);
-                checkedTurn = false;
-            }
-            else
-            {
-                if (!checkedTurn)
-                {
-                    Vector3 from = _pathCorners[index] - transform.position;
-                    Vector3 to = transform.up;
-                    float turnAngle = Vector3.SignedAngle(from, to, transform.forward);
-                    if (turnAngle == 90f)
-                    {
-                        turnDir = xMove;
-                    }
-                    else if (turnAngle == -90f)
-                    {
-                        turnDir = -xMove;
-                    }
-                    checkedTurn = true;
-                }
-                _animator.SetFloat("y", 0f, dampTime, Time.deltaTime);
-                _animator.SetFloat("x", turnDir, dampTime, Time.deltaTime);
-            }
-        }
-        else if (!shouldMove && Vector3.Distance(transform.position, _movePosition.position) > 0.5f)
-        {
-            shouldMove = true;
-        }
-        else
-        {
-            _animator.SetFloat("y", Mathf.SmoothStep(_animator.GetFloat("y"), 0f, stepTime));
-            _animator.SetFloat("x", Mathf.SmoothStep(_animator.GetFloat("x"), 0f, stepTime));
-        }
+#endif
     }
 }
